45bc6c4874e3994b3979fa5a140095ff8a813959
Alexander <36698610+Oleksandr-Ryabishchuk@users.noreply.github.com>
2018-07-09 01:25:50 +0300

    Just piece of work

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Weather()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherMvc()
+        {
+            return View();
+        }
+        public ActionResult WeatherKnockout()
+        {
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            var weath = new Weather();
+            return Json(weath.GetCurrentWeather(), JsonRequestBehavior.AllowGet);
+        }
+
+       
+    }
+}
